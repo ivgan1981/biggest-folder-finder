452b8c78155c0e1ddde7074db67e058bef7c9287
Evgeny Kostylev <ivgan1981@gmail.com>
2023-08-20 01:26:16 +0300

    fork-join

---
+++ src/main/java/FolderSizeCalculator.java
@@ -0,0 +1,38 @@
+import javax.swing.plaf.metal.MetalIconFactory;
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.RecursiveTask;
+
+public class FolderSizeCalculator extends RecursiveTask<Long>{
+
+    File folder;
+
+    public FolderSizeCalculator(File folder) {
+        this.folder = folder;
+    }
+
+    @Override
+    protected Long compute() {
+
+        if(folder.isFile()){
+            return folder.length();
+        }
+
+        long sum = 0;
+
+        List<FolderSizeCalculator> subTasks = new LinkedList<>();
+        File[] files = folder.listFiles();
+        for(File file : files){
+            FolderSizeCalculator task = new FolderSizeCalculator(file);
+            task.fork();//асинхронный запуск
+            subTasks.add(task);
+        }
+
+        for(FolderSizeCalculator task : subTasks){
+            sum += task.join();
+        }
+
+        return sum;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,7 +7,17 @@ public class Main {
         String folderPath = "D:\\Downloads";
         File file = new File(folderPath);
 
-        System.out.println(getFolderSize(file));
+        long start = System.currentTimeMillis();
+
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+
+        //System.out.println(getFolderSize(file));
+
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration + " ms");
 
     }
 
